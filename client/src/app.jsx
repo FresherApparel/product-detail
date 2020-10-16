4ec613bac33e9e6c12ca06971baf9251fc965d3b
Erik Shelton <erik.shelton328@gmail.com>
2020-10-16 15:30:58 -0600

    Set up routes in server.js

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -22,49 +22,49 @@ const App = () => {
   const [reviews, setReviews] = useState({});
   const [cart, setCart] = useState([]);
   const [productList, setProductList] = useState([]);
-  const [sessionId, setSessionId] = useState(9095);
+  const [sessionId, setSessionId] = useState(9098);
 
   useEffect(() => {
-    getAllStyles(productId, (error, response) => {
+    getAllStyles(productId, (error, response) => { // products/:product_id/styles
       if (error) {
         return 'Could not get styles';
       }
-      return setStyles(response);
+      return setStyles(response); // - styles
     });
-    getInfo(productId, (error, response) => {
+    getInfo(productId, (error, response) => { // /products/:product_id/
       if (error) {
         return 'Could not get products';
       }
-      return setProduct(response);
+      return setProduct(response); // - product
     });
-    getReviews(productId, (error, response) => {
+    getReviews(productId, (error, response) => { // /reviews/:products_id/meta
       if (error) {
         return 'Could not get reviews';
       }
-      return setReviews(response.ratings);
+      return setReviews(response.ratings); // - reviews
     });
-    getDefaultStyle(productId, (error, response) => {
+    getDefaultStyle(productId, (error, response) => { // /products/:product_id/styles
       if (error) {
         return 'Could not get default style';
       }
-      return setCurrentStyle(response);
+      return setCurrentStyle(response); // - current style (default)
     });
-    getCart(sessionId, (error, response) => {
+    getCart(sessionId, (error, response) => { // /cart/:session_id
       if (error) {
         return 'Could not get cart';
       }
       if (cart.length > 15) {
-        setSessionId((sessionId) + 1);
+        setSessionId((sessionId) + 1); // - why when the cart has more than 15 items?
       }
       return setCart(response);
     });
-    listProducts((error, response) => {
+    listProducts((error, response) => { // /products/list/
       if (error) {
         return 'Could not get product list';
       }
-      return setProductList(response);
+      return setProductList(response); // info requested but never used || passed to navbar
     });
-    setSessionId(9095);
+    setSessionId(9098); // why setting this to 9095 after every use effect
   }, [sessionId, productId, cart]);
 
   if (
@@ -91,7 +91,7 @@ const App = () => {
         <Row>
           <Col xs={10}>
             <Navigation
-              productList={productList}
+              productList={productList} // never used
               styles={styles}
             />
           </Col>
--- server/server.js
+++ server/server.js
@@ -5,6 +5,42 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.static('../client/dist'));
+app.use(express.json());
+
+// GET /products/list/
+app.get('/products/lists/', (req, res) => {
+  res.staus(200)
+    .send('Getting product list');
+});
+// GET /products/:product_id
+app.get('/products/:product_id', (req, res) => {
+  const id = req.params.product_id;
+  res.status(200)
+    .send(`Getting product by id:${id}`);
+});
+// GET /products/:product_id/styles
+app.get('/products/:product_id/styles', (req, res) => {
+  const id = req.params.product_id;
+  res.status(200)
+    .send(`Getting product styles by id:${id}`);
+});
+// GET /reviews/:product_id/meta
+app.get('/reviews/:product_id/meta', (req, res) => {
+  const id = req.params.product_id;
+  res.status(200)
+    .send(`Getting review meta data by id:${id}`);
+});
+// GET /cart/:session_id
+app.get('/cart/:session_id', (req, res) => {
+  const sid = req.params.session_id;
+  res.status(200)
+    .send(`Getting cart data by session id:${sid}`);
+});
+// POST /cart/
+app.post('/cart/', (req, res) => {
+  res.status(200)
+    .send('Adding item to cart');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running and listening on port: ${PORT}`);
